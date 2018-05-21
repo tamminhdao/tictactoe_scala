f30189ede8b124ee7913437e1ad772b86bb8e93e
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-05-21 16:07:23 -0500

    Test unbeatable AI never loses

--- src/test/scala/com/tamminhdao/ttt/GameSpec.scala
+++ src/test/scala/com/tamminhdao/ttt/GameSpec.scala
@@ -50,6 +50,7 @@ class GameSpec extends FunSpec {
       assert(Board.winner(finalBoard) == 'O)
     }
 
+
     it("between an easy AI and an unbeatable AI: end in a win for the unbeatable AI") {
       val ttt = new Game()
       val emptyBoard = Board.newBoard(3)
@@ -59,13 +60,47 @@ class GameSpec extends FunSpec {
       val symbols = List('O, 'X)
       val finalBoard = ttt.play(emptyBoard, players, symbols, MockIO)
       val expectedBoard = Vector(
-        'O, 'O, 'X,
-        'O, 'X, '_,
-        'X, '_, '_
+      'O, 'O, 'X,
+      'O, 'X, '_,
+      'X, '_, '_
       )
       assert(finalBoard == expectedBoard)
       assert(Rule.hasWinner(finalBoard))
       assert(Board.winner(finalBoard) == 'X)
+    }
+
+    it("the unbeatable AI never loses to the opportunistic AI - unbeatable AI goes first") {
+      var unbeatableAiNeverLoses = true
+      for (_ <- 1 to 50) {
+        val ttt = new Game()
+        val emptyBoard = Board.newBoard(3)
+        val player1 = new AIPlayer()
+        val player2 = new OpportunisticAI()
+        val players = List(player1, player2)
+        val symbols = List('O, 'X)
+        val finalBoard = ttt.play(emptyBoard, players, symbols, MockIO)
+        if (Board.winner(finalBoard) == 'X) {
+          unbeatableAiNeverLoses = false
+        }
+      }
+      assert(unbeatableAiNeverLoses)
+    }
+
+    it("the unbeatable AI never loses to the opportunistic AI - opportunistic AI goes first") {
+      var unbeatableAiNeverLoses = true
+      for (_ <- 1 to 50) {
+        val ttt = new Game()
+        val emptyBoard = Board.newBoard(3)
+        val player1 = new OpportunisticAI()
+        val player2 = new AIPlayer()
+        val players = List(player1, player2)
+        val symbols = List('O, 'X)
+        val finalBoard = ttt.play(emptyBoard, players, symbols, MockIO)
+        if (Board.winner(finalBoard) == 'O) {
+          unbeatableAiNeverLoses = false
+        }
+      }
+      assert(unbeatableAiNeverLoses)
     }
   }
 }
