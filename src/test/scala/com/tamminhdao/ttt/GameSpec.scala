db39478b2b0b56fabfab39027bc841a4525a059b
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-06-09 09:31:43 -0500

    Tests do not need to be ran 50 times

--- src/test/scala/com/tamminhdao/ttt/GameSpec.scala
+++ src/test/scala/com/tamminhdao/ttt/GameSpec.scala
@@ -71,37 +71,27 @@ class GameSpec extends FunSpec {
     }
 
     it("the unbeatable AI never loses to the opportunistic AI - unbeatable AI goes first") {
-      var unbeatableAiNeverLoses = true
-      for (_ <- 1 to 50) {
-        val ttt = new Game()
-        val emptyBoard = Board.newBoard(3)
-        val player1 = new UnbeatableAI()
-        val player2 = new OpportunisticAI()
-        val players = List(player1, player2)
-        val symbols = List('O, 'X)
-        val finalBoard = ttt.play(emptyBoard, players, symbols, MockIO)
-        if (Board.winner(finalBoard) == 'X) {
-          unbeatableAiNeverLoses = false
-        }
-      }
-      assert(unbeatableAiNeverLoses)
+      val ttt = new Game()
+      val emptyBoard = Board.newBoard(3)
+      val player1 = new UnbeatableAI()
+      val player2 = new OpportunisticAI()
+      val players = List(player1, player2)
+      val symbols = List('O, 'X)
+      val finalBoard = ttt.play(emptyBoard, players, symbols, MockIO)
+
+      assert(Board.winner(finalBoard) == 'O)
     }
 
     it("the unbeatable AI never loses to the opportunistic AI - opportunistic AI goes first") {
-      var unbeatableAiNeverLoses = true
-      for (_ <- 1 to 50) {
-        val ttt = new Game()
-        val emptyBoard = Board.newBoard(3)
-        val player1 = new OpportunisticAI()
-        val player2 = new UnbeatableAI()
-        val players = List(player1, player2)
-        val symbols = List('O, 'X)
-        val finalBoard = ttt.play(emptyBoard, players, symbols, MockIO)
-        if (Board.winner(finalBoard) == 'O) {
-          unbeatableAiNeverLoses = false
-        }
-      }
-      assert(unbeatableAiNeverLoses)
+      val ttt = new Game()
+      val emptyBoard = Board.newBoard(3)
+      val player1 = new OpportunisticAI()
+      val player2 = new UnbeatableAI()
+      val players = List(player1, player2)
+      val symbols = List('O, 'X)
+      val finalBoard = ttt.play(emptyBoard, players, symbols, MockIO)
+
+      assert(Board.winner(finalBoard) != 'O)
     }
   }
 }
